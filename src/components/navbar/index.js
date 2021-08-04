4c0840bd66b71ecdd54b37e7e92493e85af013a4
Connor Harrington <csharrington@berkeley.edu>
2021-08-03 18:37:23 -0700

    testing commit

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React from "react";
 
 import { Link } from "react-router-dom";
 
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
+
+/*
+    --- NAVBAR CONTENTS ---
+    1. Logo/name that links to landing page --> Look into Bootstrap's Navbar.Brand
+    2. Links --> Boostrap's Navs
+        - Home
+        - Offerings
+        - Get Involved
+        - Our People
+        - Contact us
+*/
 
 const SiteNavbar = () => {
-    return(
-        <Navbar bg="light" expand="lg">
-            <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                <Link to="/">Home</Link>
-                <Link to="/contact">Contact</Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link to="/">Home</Nav.Link>
+            <Nav.Link to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default SiteNavbar
+export default SiteNavbar;
