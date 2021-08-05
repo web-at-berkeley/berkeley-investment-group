71bf97668f77369c64f73efc442571d63f8027a7
Connor Harrington <csharrington@berkeley.edu>
2021-08-04 22:58:38 -0700

    Aug 4, still playing with bootstrap

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,6 +7,7 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
 import styles from "./styles.module.scss";
+import logo from "./big-logo.svg";
 
 /*
     --- NAVBAR CONTENTS ---
@@ -23,12 +24,17 @@ const SiteNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={logo} height="60rem" alt="Berkeley Investment Group" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="ms-auto">
             <Nav.Link to="/">Home</Nav.Link>
-            <Nav.Link to="/contact">Contact</Nav.Link>
+            <Nav.Link to="/offerings">Offerings</Nav.Link>
+            <Nav.Link to="/involve">Get Involved</Nav.Link>
+            <Nav.Link to="/people">Our People</Nav.Link>
+            <Nav.Link to="/contact">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+import bannerImg from "./banner-img.jpeg";
+/*
+    --- LANDING PAGE CONTENT ---
+    1. Navbar
+    2. Banner Image
+    3. Quotes | Group Photo
+    4. Events graphic | Event info
+    5. "What we do"
+    6. Footer
+*/
 
 const Landing = () => {
-    return(
-        <div>
-            <h1>Landing</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
 
-export default Landing
+const Banner = () => {
+  return (
+    <Container overflow>
+      <Card class="text-white">
+        <Card.Img src={bannerImg} />
+        <Card.ImgOverlay>
+          <Card.Title>Berkeley Investment Group</Card.Title>
+          <Card.Text>Invest in yourself.</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </Container>
+  );
+};
+
+export default Landing;
